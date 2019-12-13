a5d24d387e44386ebc8166178093277ef0f1495f
te170476 <te170476@gmail.com>
2019-12-13 14:38:19 +0900

    add OleDbExtensions#Connect()

--- CSACC/extension/OleDbExtensions.cs
+++ CSACC/extension/OleDbExtensions.cs
@@ -1,12 +1,30 @@
-﻿using System;
+﻿using com.github.tcc170476.CSACC.util;
+using System;
 using System.Collections.Generic;
+using System.Data.OleDb;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace com.github.tcc170476.CSACC.extension
 {
-    class OleDbExtensions
+    public static class OleDbExtensions
     {
+        public static Option<OleDbConnection> Connect(this OleDbConnection connection)
+        {
+            try
+            {
+                connection.Open();
+            }
+            catch (OleDbException exception)
+            {
+                return new None<OleDbConnection>("Error: Failed connection. " + exception.Message);
+            }
+            catch (InvalidOperationException exception) // Already connected.
+            {
+                return new None<OleDbConnection>("Warning: Already connected. " + exception.Message);
+            }
+            return new Some<OleDbConnection>(connection);
+        }
     }
 }
