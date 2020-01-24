af30dc65341cd76f441b43876159cbc128a7c09e
te170476 <te170476@gmail.com>
2020-01-24 13:55:06 +0900

    add input.fromCsv.ToEntityConverter

---
+++ CSACC/input/fromCsv/ToEntityConverter.cs
@@ -0,0 +1,28 @@
+﻿using CSACC.entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSACC.input.fromCsv
+{
+    class ToEntityConverter
+    {
+        public List<Request> ToRequest(CsvLine csvLine)
+        {
+            // TODO:
+            //   CsvLine
+        }
+        private List<WorkTimeDivision> getWorkTimeDivision(String str)
+        {
+            switch (str)
+            {
+                case "午前": return new List<WorkTimeDivision>() { WorkTimeDivision.AM };
+                case "午後": return new List<WorkTimeDivision>() { WorkTimeDivision.PM };
+                case "１日": return new List<WorkTimeDivision>() { WorkTimeDivision.AM, WorkTimeDivision.PM };
+                default: return new List<WorkTimeDivision>() { };
+            }
+        }
+    }
+}
