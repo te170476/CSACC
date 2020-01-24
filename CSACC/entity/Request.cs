95ed0c2a90349682f0c6bec91463646e57ea798f
te170476 <te170476@gmail.com>
2020-01-24 13:41:25 +0900

    add entity.Request

---
+++ CSACC/entity/Request.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSACC.entity
+{
+    class Request
+    {
+        public String   Requester   { get; protected set; }
+        public DateTime WorkDate    { get; protected set; }
+        public String   WorkTime    { get; protected set; }
+        public DateTime RestDate    { get; protected set; }
+        public String   RestTime    { get; protected set; }
+        public String   Remarks     { get; protected set; }
+
+        public Request(
+              String    requester
+            , DateTime  workDate
+            , String    workTime
+            , DateTime  restDate
+            , String    restTime
+            , String    remarks
+            )
+        {
+            Requester   = requester;
+            WorkDate    = workDate;
+            WorkTime    = workTime;
+            RestDate    = restDate;
+            RestTime    = restTime;
+            Remarks     = remarks;
+        }
+        public override string ToString()
+        {
+            return $"{Requester},{WorkDate.ToShortDateString()},{WorkTime}"
+                            + $",{RestDate.ToShortDateString()},{RestTime}"
+                            + $",{Remarks}";
+        }
+    }
+}
