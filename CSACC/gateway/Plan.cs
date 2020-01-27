18133080b3f9cadb33be00d8db7e246ef06be1ba
te170476 <te170476@gmail.com>
2020-01-27 10:25:46 +0900

    add gateway.Plan

---
+++ CSACC/gateway/Plan.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSACC.gateway
+{
+    class Plan
+    {
+        public String Requester { get; protected set; }
+        public String Date      { get; protected set; }
+        public String Time      { get; protected set; }
+        public override string ToString() { return $"{Requester},{Date},{Time}"; }
+
+        public class Work : Plan
+        {
+            public Rest RestPlan { get; protected set; }
+            public Work(
+                  String    requester
+                , String    date
+                , String    time
+                , Rest      restPlan
+                )
+            {
+                Requester   = requester;
+                Date        = date;
+                Time        = time;
+                RestPlan    = restPlan;
+            }
+            public override string ToString() { return $"work,{base.ToString()}"; }
+        }
+        public class Rest : Plan
+        {
+            public Rest(
+                  String requester
+                , String date
+                , String time
+                )
+            {
+                Requester   = requester;
+                Date        = date;
+                Time        = time;
+            }
+            public override string ToString() { return $"rest,{base.ToString()}"; }
+        }
+    }
+}
