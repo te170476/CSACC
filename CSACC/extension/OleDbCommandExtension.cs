334a1752ac1a5807c7c5a0de6f91013a3e3c06ea
te170476 <te170476@gmail.com>
2020-02-03 11:33:17 +0900

    add OleDbCommandExtension

---
+++ CSACC/extension/OleDbCommandExtension.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSACC.extension
+{
+    class OleDbExtension
+    {
+        public static Option<int> SelectIDENTITY(OleDbConnection connection, OleDbTransaction transaction)
+        {
+            var selectCommand = new OleDbCommand("SELECT @@IDENTITY", connection);
+            selectCommand.Transaction = transaction;
+            return selectCommand.OptionalExecuteScalar<int>().getOptRight();
+        }
+    }
+    static class OleDbCommandExtension
+    {
+        public static Either<Exception, int> OptionalExecuteNonQuery(this OleDbCommand self)
+        {
+            try
+            {
+                return new Right<Exception, int>(self.ExecuteNonQuery());
+            }
+            catch (InvalidOperationException exception)
+            {
+                return new Left<Exception, int>(exception);
+            }
+        }
+        public static Either<Exception, T> OptionalExecuteScalar<T>(this OleDbCommand self)
+        {
+            try
+            {
+                return new Right<Exception, T>(self.ExecuteScalar().OptionalCast<T>().get());
+            }
+            catch (InvalidOperationException exception)
+            {
+                return new Left<Exception, T>(exception);
+            }
+        }
+    }
+}
