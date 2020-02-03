e5dd4dfd73299371a82e21c32f3a8c6361d592fe
te170476 <te170476@gmail.com>
2020-02-03 11:33:26 +0900

    add ParseExtension

---
+++ CSACC/extension/ParseExtension.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSACC.extension
+{
+    public static class ParseExtension
+    {
+        public static Option<O> OptionalCast<I, O>(this Option<I> source)
+        {
+            try
+            {
+                return source.get().OptionalCast<O>();
+            }
+            catch (Exception exception)
+            {
+                return new None<O>();
+            }
+        }
+        public static Option<T> OptionalCast<T>(this Object source)
+        {
+            try
+            {
+                return new Some<T>((T)source);
+            }
+            catch (Exception exception)
+            {
+                return new None<T>();
+            }
+            //if(source is T)
+            //    return new Some<T>((T)source);
+            //return new None<T>();
+        }
+    }
+}
