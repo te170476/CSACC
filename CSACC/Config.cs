9b71ac4a295cf836a8930ea207b0ec6bdf0f3c9f
te170476 <te170476@gmail.com>
2020-02-14 16:44:17 +0900

    add Config.cs

---
+++ CSACC/Config.cs
@@ -0,0 +1,16 @@
+﻿using CSACC.Properties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace CSACC
+{
+    class Config
+    {
+        public static String DbProvider = ConfigurationManager.AppSettings.Get("databaseProvider");
+        public static String DbSource   = ConfigurationManager.AppSettings.Get("databasePath");
+    }
+}
