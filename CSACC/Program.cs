c085be0bd6c7e244a5c964d396a00bd4bba2881b
te170476 <te170476@gmail.com>
2020-02-14 15:45:15 +0900

    cui実行に対応

--- CSACC/Program.cs
+++ CSACC/Program.cs
@@ -12,8 +12,29 @@ namespace CSACC
         /// アプリケーションのメイン エントリ ポイントです。
         /// </summary>
         [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
+            if (args.Length > 0)
+            {
+                try
+                {
+                    var toEntity = new input.fromCsv.ToEntityConverter();
+                    var fromEntity = new gateway.FromEntityConverter();
+                    var gateway = new gateway.Gateway();
+                    args.ToList().ForEach(path =>
+                        new input.fromCsv.CsvReader().read(path)
+                            .SelectMany(it => toEntity.ToRequest(it))
+                            .Select(it => fromEntity.FromRequest(it))
+                            .ToList()
+                            .ForEach(it => gateway.Apply(it))
+                    );
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Error: "+e.StackTrace);
+                }
+                return;
+            }
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
